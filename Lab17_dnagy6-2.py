a7729e4b707975d1516791dc8f3d9fa6cf985bf2
Dakota Nagy <dnagy6@student.cscc.edu>
2026-08-11 13:21:32 -0400

    Requested API with handling using try and except, while providing temporary use of print to display rust repo results

--- Lab17_dnagy6-2.py
+++ Lab17_dnagy6-2.py
@@ -7,9 +7,32 @@ Purpose: Fetches data via the GitHub API for top-starred projects in Rust
 Starter Code: Adapted and tailored code based on tutorial provided in class lecture video.
 Date: August 14, 2026
 """
+import requests
 
+def fetch_repository_data(language: str) -> dict:
+    """Pull repo data from GitHub Rust (REST) API with exception handling."""
+    url = f"https://api.github.com/search/repositories?q=language:rust&sort=stars"
+    headers = {"Accept": "application/vnd.github.v3+json"}
+
+    try:
+        response = requests.get(url,headers=headers,timeout=10)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as error:
+        print(f"Error fetching data from GitHub API: {error}")
+        return{}
+    
 def main():
-    pass
+    target_language = "rust"
+    print(f"Pulling top {target_language.capitalize()} repos.")
+
+    response_dict = fetch_repository_data(target_language)
+
+    if response_dict:
+        print(f"Success")
+        print(f"Repos found: {response_dict.get('total_count')}")
+        print(f"Items on first page: {len(response_dict.get('items', []))}")
+
 
 if __name__ == "__main__":
     main()
