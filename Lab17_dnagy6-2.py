dfd1a4a144588d67c63eb90f0c71fb9555399adc
Dakota Nagy <dnagy6@student.cscc.edu>
2026-08-11 13:00:52 -0400

    Created initial work environment by installing requests, plotly, and pandas. Created .gitingore file.

---
+++ Lab17_dnagy6-2.py
@@ -0,0 +1,15 @@
+"""
+Program Name: Top GitHub Rust Repositories Visual
+Author: Dakota Nagy
+Purpose: Fetches data via the GitHub API for top-starred projects in Rust
+        programming language, processes the JSON response, and generates an 
+        interactive Plotly HTML bar chart.
+Starter Code: Adapted and tailored code based on tutorial provided in class lecture video.
+Date: August 14, 2026
+"""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
